44a2800e4a7a85d59854cf8202ad6609073b7562
NaokiHigashida <git@github.com:higashida-naoki/meshiterro.git>
2025-02-15 04:51:31 +0000

    header ハンバーガーメニュー

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,10 +4,9 @@
 // that code so it'll be compiled.
 
 import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
+// import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-
 import "jquery";
 import "popper.js";
 import "bootstrap";
@@ -15,5 +14,34 @@ import "../stylesheets/application";
 
 
 Rails.start()
-Turbolinks.start()
 ActiveStorage.start()
+
+document.addEventListener("DOMContentLoaded", function() {
+  const menuToggle = document.getElementById("menu-toggle");
+  const menuClose = document.getElementById("menu-close");
+
+  const menu = document.getElementById("menu");
+
+  // ページ読み込み後にメニューの状態を復元
+  if (sessionStorage.getItem('menuOpen') === 'true') {
+    menu.classList.add('open');
+  }
+
+  if (menuToggle && menuClose && menu) {
+    menuToggle.addEventListener("click", function() {
+      menu.classList.toggle("open");
+      sessionStorage.setItem('menuOpen', menu.classList.contains('open'));
+    });
+
+    menuClose.addEventListener("click", function() {
+      menu.classList.remove("open");
+      sessionStorage.setItem('menuOpen', false);
+    });
+    $('.menu-link').on("click", function() {
+      menu.classList.remove("open");
+      sessionStorage.setItem('menuOpen', false);
+    });
+  }
+});
+
+
