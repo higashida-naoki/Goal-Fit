6778c7a68de6e7362961e9d784374e7a29f81bbc
NaokiHigashida <git@github.com:higashida-naoki/meshiterro.git>
2025-02-19 06:31:48 +0000

    layout

--- app/controllers/admin_side/post_comments_controller.rb
+++ app/controllers/admin_side/post_comments_controller.rb
@@ -1,21 +1,10 @@
-# app/controllers/admin/comments_controller.rb
-class Admin::CommentsController < ApplicationController
+# app/controllers/admin_side/post_comments_controller.rb
+class AdminSide::PostCommentsController < ApplicationController
   before_action :authenticate_admin!
-  before_action :set_post
-  before_action :set_comment, only: [:destroy]
 
   def destroy
-    @comment.destroy
-    redirect_to admin_post_path(@post), notice: 'コメントが削除されました。'
-  end
-
-  private
-
-  def set_post
-    @post = Post.find(params[:post_id])
-  end
-
-  def set_comment
-    @comment = @post.post_comments.find(params[:id])
+    @post_comment = PostComment.find(params[:id])
+    @post_comment.destroy
+    redirect_to admin_side_post_path(@post_comment.post_id)
   end
 end
