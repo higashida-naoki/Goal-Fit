a937c7f2165bf745544a7421f697a47e51f20661
NaokiHigashida <git@github.com:higashida-naoki/meshiterro.git>
2025-02-20 12:03:33 +0000

    2ndデプロイ+テスト完了

--- app/controllers/user_side/relationships_controller.rb
+++ app/controllers/user_side/relationships_controller.rb
@@ -1,25 +1,30 @@
 class UserSide::RelationshipsController < ApplicationController
   before_action :authenticate_user!
-  def create
-    user = User.find(params[:user_id])
-    current_user.follow(user)
-		redirect_to request.referer
-  end
-  
-  def destroy
-    user = User.find(params[:user_id])
-    current_user.unfollow(user)
-		redirect_to request.referer
-  end
-  
+  before_action :set_user, only: [:followings, :followers]
+
   def followings
-    user = User.find(params[:user_id])
-		@users = user.followings
+    @followings = @user.followings
   end
 
   def followers
+    @followers = @user.followers
+  end
+
+  def create
     user = User.find(params[:user_id])
-		@users = user.followers
+    current_user.follow(user)
+    redirect_to request.referer
+  end
+
+  def destroy
+    user = User.find(params[:user_id])
+    current_user.unfollow(user)
+    redirect_to request.referer
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:user_id])
   end
 end
-
